2419621916328dde85f2fddd3f474ad4cbce18b2
Khooshal Seechurn <khooshalseechurn@gmail.com>
2021-08-03 13:14:12 +0400

    Validations added

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,7 @@
 class Trip < ApplicationRecord
   belongs_to :user
   has_many :bookings, through: :users
+  validates :title, presence: true
+  validates :type, presence: true
+  validates :price, presence: true
 end
