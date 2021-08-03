a6c264da0207e3ae8a40785af0726b1b5abae7e0
Khooshal Seechurn <khooshalseechurn@gmail.com>
2021-08-03 11:03:02 +0400

    added bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,5 +5,15 @@ class BookingsController < ApplicationController
 
   def new
     @trip = Trip.find(params[:trip_id])
+    @booking = Booking.new(booking_params)
+    @booking.trip = @trip
+    # if @booking.save
+    #   redirect_to
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :start_date, :end_date)
   end
 end
