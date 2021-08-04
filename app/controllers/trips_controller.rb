61d6682a686650d80c6c714a417b8a8ffa21d2b3
Nasreen Dowlut <nasmdow@gmail.com>
2021-08-04 09:24:38 +0100

    changes to links

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,5 @@
 class TripsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @trips = Trip.all
@@ -8,4 +9,40 @@ class TripsController < ApplicationController
     @trip = Trip.find(params[:id])
   end
 
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.user = current_user
+    if @trip.save
+      redirect_to trips_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @trip = Trip.find(trip[:id])
+    @trip.destroy
+    redirect_to trips_path
+  end
+
+  def edit
+    @trip = trip.find(params[:id])
+  end
+
+  def update
+    @trip = trip.find(params[:id])
+    @trip.update(trip_params)
+    redirect_to trips_path
+  end
+
+  private
+
+  def trip_params
+   params.require(:trip).permit(:title, :category, :details, :total_participant, :price)
+  end  
+  
 end
