4aad3df0e6848b0da4f786a160c42688533d2bce
Nasreen Dowlut <nasmdow@gmail.com>
2021-08-04 11:54:11 +0100

    solve problems on local

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-    resources :trips do
+  authenticate :users do
+    
+    resources :trips, only: [:new, :create] do 
       resources :bookings, only: [:new, :create]
     end
     resources :bookings, only: [:index, :update, :edit, :destroy]
+  end
+  
 end
 
