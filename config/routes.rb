1f7593822814205ae980a19b1d53b778dd45e9ea
Nasreen Dowlut <nasmdow@gmail.com>
2021-08-04 13:43:48 +0100

    routes not pulled properly from git

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  authenticate :users do
     
-    resources :trips, only: [:new, :create] do 
+    resources :trips do 
       resources :bookings, only: [:new, :create]
     end
     resources :bookings, only: [:index, :update, :edit, :destroy]
-  end
   
 end
 
